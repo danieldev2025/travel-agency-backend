68bc8e09df58a8569370d732c99f98a09e4e9e8d
danieldev2025 <danielguesh8888@gmail.com>
2025-05-01 14:20:24 +0300

    remove cors restrictions

--- app.js
+++ app.js
@@ -19,12 +19,7 @@ app.use(nocache()); // Prevent caching
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
-// CORS Configuration
-// const corsOptions = {
-//   origin: "*",
-//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//   allowedHeaders: ["Content-Type", "Authorization"],
-// };
+
 app.use(cors());
 
 // Check database connection
